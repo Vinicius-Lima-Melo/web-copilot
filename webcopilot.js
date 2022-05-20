25b6d5e122f5e84a775e46357d3ee3b421403296
Vinicius Lima <viniciuslimamelo0@gmail.com>
2022-05-20 13:37:45 -0300

    Get values on Input keyup

--- webcopilot.js
+++ webcopilot.js
@@ -1,1 +1,8 @@
-document.getElementsByTagName('h1')[0].innerHTML = 'WebCopilot';
+// document.getElementsByTagName('body')[0].append('<div style="background: blue; border: 1px solid blue"></div>');
+// document.getElementsByTagName('h1')[0].innerHTML = 'WebCopilot';
+
+document.querySelectorAll('input').forEach( input => {
+  input.addEventListener('keyup', function(e) {
+      console.log("extension > ", e.target.value);
+  })
+});
