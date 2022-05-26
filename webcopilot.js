693b6077f2ff56dc456708f6bfd7e2039d1df6e3
Vinicius Lima <viniciuslimamelo0@gmail.com>
2022-05-26 20:16:25 -0300

    Update webcopilot.js

--- webcopilot.js
+++ webcopilot.js
@@ -1,8 +1,36 @@
-// document.getElementsByTagName('body')[0].append('<div style="background: blue; border: 1px solid blue"></div>');
-// document.getElementsByTagName('h1')[0].innerHTML = 'WebCopilot';
+let types = {
+  userNames: {
+    [0]: "Vinicius",    
+    [1]: "Lucas",    
+    [2]: "João",    
+    [3]: "Marcelo",    
+  },
+  telefones: {
+    [0]: "(15) 99852-7892",    
+    [1]: "(11) 99734-2233",    
+    [2]: "(31) 99059-2301",    
+    [3]: "(88) 99756-8459",
+  },
+  ceps:{
+    [0]: "18044-200",    
+    [1]: "60731-280",    
+    [2]: "24913-505",    
+    [3]: "49020-085",
+  },
+  cpfs:{
+    [0]: "351.936.550-25",    
+    [1]: "750.061.420-90",    
+    [2]: "946.404.840-97",    
+    [3]: "340.310.780-99",
+  }
+}
+
+console.log(types)
+
 
 document.querySelectorAll('input').forEach( input => {
-  input.addEventListener('keyup', function(e) {
-      console.log("extension > ", e.target.value);
-  })
+  let el = input
+  let index = Math.floor(Math.random() * 4)
+
+  input.value = types[el.ariaLabel+'s'][index]
 });
