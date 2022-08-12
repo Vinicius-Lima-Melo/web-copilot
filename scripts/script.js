238d90624fd46a6c04f57fcc7705c8652ae8482e
vinicius-lima-melo <viniciuslimamelo0@gmail.com>
2022-08-12 03:35:03 -0300

    Obtendo e aplicando configurações do popup

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,18 @@
-$(document).on('change', '#sugestoes', function() {
-    localStorage.setItem('sugestoes', $(this).val());
-    // console.log("Produto selecionado: "+ $(this).attr('id'));		
+$(document).ready(function () {
+    chrome.storage.sync.get(["WC_autocomplete", "WC_show_suggestions"], function (items) {
+        $('#WC_autocomplete').prop('checked', items.WC_autocomplete);
+        $('#WC_show_suggestions').prop('checked', items.WC_show_suggestions);
+    });
+})
+
+$(document).on('change', '#WC_show_suggestions', function(e) {
+    const {checked} = e.target
+    chrome.storage.sync.set({WC_show_suggestions: checked});
+    alert("Atualize a página para aplicar as alterações")
+}); 
+
+$(document).on('change', '#WC_autocomplete', function(e) {
+    const {checked} = e.target
+    chrome.storage.sync.set({WC_autocomplete: checked});
+    alert("Atualize a página para aplicar as alterações")
 }); 
--- webcopilot.js
+++ webcopilot.js
@@ -1,33 +1,51 @@
-let types = {
-  userNames: {
-    [0]: "Vinicius",    
-    [1]: "Lucas",    
-    [2]: "João",    
-    [3]: "Marcelo",    
-  },
-  telefones: {
-    [0]: "(15) 99852-7892",    
-    [1]: "(11) 99734-2233",    
-    [2]: "(31) 99059-2301",    
-    [3]: "(88) 99756-8459",
-  },
-  ceps:{
-    [0]: "18044-200",    
-    [1]: "60731-280",    
-    [2]: "24913-505",    
-    [3]: "49020-085",
-  },
-  cpfs:{
-    [0]: "351.936.550-25",    
-    [1]: "750.061.420-90",    
-    [2]: "946.404.840-97",    
-    [3]: "340.310.780-99",
+console.log("WC - webcopilot.js");
+console.log("WC > running...");
+
+
+
+
+chrome.storage.sync.get(["WC_autocomplete", "WC_show_suggestions"], function (items) {
+  if(items.WC_autocomplete) {
+    executeScript();
   }
-}
-
-document.querySelectorAll('input').forEach( input => {
-  let el = input
-  let index = Math.floor(Math.random() * 4)
-
-  input.value = types[el.ariaLabel+'s'][index]
+  else{
+    console.log("WC > WC_autocomplete is disabled");
+  }
 });
+
+
+function executeScript(){
+  let types = {
+    userNames: {
+      [0]: "Vinicius",
+      [1]: "Lucas",
+      [2]: "João",
+      [3]: "Marcelo",
+    },
+    telefones: {
+      [0]: "(15) 99852-7892",
+      [1]: "(11) 99734-2233",
+      [2]: "(31) 99059-2301",
+      [3]: "(88) 99756-8459",
+    },
+    ceps: {
+      [0]: "18044-200",
+      [1]: "60731-280",
+      [2]: "24913-505",
+      [3]: "49020-085",
+    },
+    cpfs: {
+      [0]: "351.936.550-25",
+      [1]: "750.061.420-90",
+      [2]: "946.404.840-97",
+      [3]: "340.310.780-99",
+    },
+  };
+  
+  document.querySelectorAll("input").forEach((element) => {
+    if (element.ariaLabel === null || element.ariaLabel === "") return;
+    element.style.borderColor = "#0F0";
+    let index = Math.floor(Math.random() * 4);
+    element.value = types[element.ariaLabel + "s"][index];
+  });
+}
